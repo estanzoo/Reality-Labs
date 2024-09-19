dd985cb16d24e5a69a7d66efc5086cd457c95640
estanzoo <mysupppermail@gmail.com>
2024-09-19 19:30:19 -0400

    score system

--- Rollaball-v2/Assets/Scripts/PlayerController.cs
+++ Rollaball-v2/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     void Start()
     {
+        SetCountText();
+        count = 0;
         rb = GetComponent <Rigidbody>();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue){
@@ -21,9 +28,25 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if (count >= 7){
+            winTextObject.SetActive(true);
+        }
+    }
     void FixedUpdate(){
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter (Collider other){
+
+        if (other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
     }
 
 }
